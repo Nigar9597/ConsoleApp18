03926f0df2b85ca7c54e0b1ed73f3642d624c6e1
@Nigar9597 <@nigarza-bb208@code.edu.az>
2025-04-18 01:19:18 +0400

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Net.WebRequestMethods;
+using System.IO;
+using System.Text.Json;
+using System.Net;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+
+
+        static async Task Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo("Users");
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+            FileInfo file = new FileInfo(Path.Combine(directory.FullName, "users.join"));
+            if (!file.Exists)
+            {
+                file.Create();
+            }
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+                response.EnsureSuccessStatusCode();
+
+                string jsonContent = await response.Content.ReadAsStringAsync();
+
+
+                List<User> users = JsonSerializer.Deserialize<List<User>>(jsonContent);
+
+
+
+
+            }
+        }
+    }
+}
